fcb094a39dbfbfab6076af8ca0e362bfaf5ae887
saprvade <saprvade@MacBook-Pro-saprvade.local>
2016-09-10 18:59:06 -0700

    - implement

--- network-manager-start/app/src/main/java/com/google/codelab/networkmanager/MainActivity.java
+++ network-manager-start/app/src/main/java/com/google/codelab/networkmanager/MainActivity.java
@@ -1,12 +1,12 @@
 /**
  * Copyright Google Inc. All Rights Reserved.
- *
- *  Licensed under the Apache License, Version 2.0 (the "License"); you may not use this file except
+ * <p>
+ * Licensed under the Apache License, Version 2.0 (the "License"); you may not use this file except
  * in compliance with the License. You may obtain a copy of the License at
- *
- *  http://www.apache.org/licenses/LICENSE-2.0
- *
- *  Unless required by applicable law or agreed to in writing, software distributed under the
+ * <p>
+ * http://www.apache.org/licenses/LICENSE-2.0
+ * <p>
+ * Unless required by applicable law or agreed to in writing, software distributed under the
  * License is distributed on an "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either
  * express or implied. See the License for the specific language governing permissions and
  * limitations under the License.
@@ -28,6 +28,9 @@ import android.view.View;
 import android.widget.Button;
 import android.widget.Toast;
 
+import com.google.android.gms.gcm.GcmNetworkManager;
+import com.google.android.gms.gcm.OneoffTask;
+
 import java.util.ArrayList;
 import java.util.Calendar;
 import java.util.List;
@@ -37,122 +40,144 @@ import java.util.List;
  */
 public class MainActivity extends AppCompatActivity {
 
-    public static final String TASK_ID_PREFIX = "task-id";
+  public static final String TASK_ID_PREFIX = "task-id";
 
-    private class LoadTask extends AsyncTask<Void, Void, List<TaskItem>> {
+  private static final String TAG = "MainActivity";
+  private LocalBroadcastManager mLocalBroadcastManager;
+  private BroadcastReceiver mBroadcastReceiver;
+  private GcmNetworkManager mGcmNetworkManager;
 
-        private Context mContext;
+  private TaskAdapter mTaskAdapter;
+  private RecyclerView mRecyclerView;
 
-        public LoadTask(Context context) {
-            mContext = context;
-        }
+  @Override
+  protected void onCreate(Bundle savedInstanceState) {
+    super.onCreate(savedInstanceState);
+    setContentView(R.layout.activity_main);
 
-        @Override
-        protected List<TaskItem> doInBackground(Void... voids) {
-            return CodelabUtil.getTaskItemsFromFile(mContext);
-        }
+    mGcmNetworkManager = GcmNetworkManager.getInstance(this);
 
-        @Override
-        protected void onPostExecute(List<TaskItem> taskItems) {
-            mTaskAdapter.setTaskItems(taskItems);
-            mTaskAdapter.notifyDataSetChanged();
-        }
-    }
+    mTaskAdapter = new TaskAdapter(new ArrayList<TaskItem>());
 
-    // AsyncTask used to add tasks to file off the UI thread.
-    private class AddTask extends AsyncTask<TaskItem, Void, TaskItem> {
+    LinearLayoutManager linearLayoutManager = new LinearLayoutManager(this);
+    mRecyclerView = (RecyclerView) findViewById(R.id.recyclerView);
+    mRecyclerView.setLayoutManager(linearLayoutManager);
+    mRecyclerView.setAdapter(mTaskAdapter);
+    Button bestTimeButton = (Button) findViewById(R.id.bestTimeButton);
+    bestTimeButton.setOnClickListener(new View.OnClickListener() {
+      @Override
+      public void onClick(View view) {
+        String taskId = TASK_ID_PREFIX +
+            Calendar.getInstance().getTimeInMillis();
+        Log.d(TAG, "Scheduling oneoff task. " + taskId);
+        TaskItem taskItem = new TaskItem(taskId, TaskItem.ONEOFF_TASK,
+            TaskItem.PENDING_STATUS);
+        new AddTask(view.getContext()).execute(taskItem);
+      }
+    });
 
-        private Context mContext;
+    Button nowButton = (Button) findViewById(R.id.nowButton);
+    nowButton.setOnClickListener(new View.OnClickListener() {
+      @Override
+      public void onClick(View view) {
+        String taskId = TASK_ID_PREFIX + Calendar.getInstance().getTimeInMillis();
+        Log.d(TAG, "Creating a Now Task. " + taskId);
+        TaskItem taskItem = new TaskItem(taskId, TaskItem.NOW_TASK, TaskItem.PENDING_STATUS);
+        new AddTask(view.getContext()).execute(taskItem);
+      }
+    });
 
-        public AddTask(Context context) {
-            mContext = context;
-        }
+    mLocalBroadcastManager = LocalBroadcastManager.getInstance(this);
+    mBroadcastReceiver = new BroadcastReceiver() {
+      @Override
+      public void onReceive(Context context, Intent intent) {
+        String taskId = intent.getStringExtra(CodelabUtil.TASK_ID);
+        String status = intent.getStringExtra(CodelabUtil.TASK_STATUS);
 
-        @Override
-        protected TaskItem doInBackground(TaskItem... taskItems) {
-            // Items are being added to the list before being scheduled/executed to allow their state to be
-            // visible in the UI.
-            TaskItem taskItem = taskItems[0];
-            CodelabUtil.addTaskItemToFile(mContext, taskItem);
-            return taskItem;
-        }
+        mTaskAdapter.updateTaskItemStatus(taskId, status);
+      }
+    };
+  }
 
-        @Override
-        protected void onPostExecute(TaskItem taskItem) {
-            mTaskAdapter.addTaskItem(taskItem);
-            mRecyclerView.scrollToPosition(0);
+  @Override
+  public void onResume() {
+    super.onResume();
+    mLocalBroadcastManager.registerReceiver(mBroadcastReceiver, new IntentFilter(CodelabUtil.TASK_UPDATE_FILTER));
+    new LoadTask(this).execute();
+  }
 
-            if (taskItem.getType().equals(TaskItem.ONEOFF_TASK)) {
-                // Schedule oneoff task.
-                Log.d(TAG, "oneoff task scheduling not implemented.");
-            } else {
-                // Immediately make network call.
-                Intent nowIntent = new Intent(mContext, NowIntentService.class);
-                nowIntent.putExtra(CodelabUtil.TASK_ID, taskItem.getId());
-                mContext.startService(nowIntent);
-            }
-        }
-    }
+  @Override
+  public void onPause() {
+    mLocalBroadcastManager.unregisterReceiver(mBroadcastReceiver);
+    super.onPause();
+  }
 
-    private static final String TAG = "MainActivity";
-    private LocalBroadcastManager mLocalBroadcastManager;
-    private BroadcastReceiver mBroadcastReceiver;
+  private class LoadTask extends AsyncTask<Void, Void, List<TaskItem>> {
 
-    private TaskAdapter mTaskAdapter;
-    private RecyclerView mRecyclerView;
+    private Context mContext;
 
-    @Override
-    protected void onCreate(Bundle savedInstanceState) {
-        super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
-
-        mTaskAdapter = new TaskAdapter(new ArrayList<TaskItem>());
-
-        LinearLayoutManager linearLayoutManager = new LinearLayoutManager(this);
-        mRecyclerView = (RecyclerView) findViewById(R.id.recyclerView);
-        mRecyclerView.setLayoutManager(linearLayoutManager);
-        mRecyclerView.setAdapter(mTaskAdapter);
-        Button bestTimeButton = (Button) findViewById(R.id.bestTimeButton);
-        bestTimeButton.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                Toast.makeText(view.getContext(), "Oneoff tasks are not implemented.", Toast.LENGTH_SHORT).show();
-            }
-        });
-
-        Button nowButton = (Button) findViewById(R.id.nowButton);
-        nowButton.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                String taskId = TASK_ID_PREFIX + Calendar.getInstance().getTimeInMillis();
-                Log.d(TAG, "Creating a Now Task. " + taskId);
-                TaskItem taskItem = new TaskItem(taskId, TaskItem.NOW_TASK, TaskItem.PENDING_STATUS);
-                new AddTask(view.getContext()).execute(taskItem);
-            }
-        });
-
-        mLocalBroadcastManager = LocalBroadcastManager.getInstance(this);
-        mBroadcastReceiver = new BroadcastReceiver() {
-            @Override
-            public void onReceive(Context context, Intent intent) {
-                String taskId = intent.getStringExtra(CodelabUtil.TASK_ID);
-                String status = intent.getStringExtra(CodelabUtil.TASK_STATUS);
-
-                mTaskAdapter.updateTaskItemStatus(taskId, status);
-            }
-        };
+    public LoadTask(Context context) {
+      mContext = context;
     }
 
     @Override
-    public void onResume() {
-        super.onResume();
-        mLocalBroadcastManager.registerReceiver(mBroadcastReceiver, new IntentFilter(CodelabUtil.TASK_UPDATE_FILTER));
-        new LoadTask(this).execute();
+    protected List<TaskItem> doInBackground(Void... voids) {
+      return CodelabUtil.getTaskItemsFromFile(mContext);
     }
 
     @Override
-    public void onPause() {
-        mLocalBroadcastManager.unregisterReceiver(mBroadcastReceiver);
-        super.onPause();
+    protected void onPostExecute(List<TaskItem> taskItems) {
+      mTaskAdapter.setTaskItems(taskItems);
+      mTaskAdapter.notifyDataSetChanged();
     }
+  }
+
+  // AsyncTask used to add tasks to file off the UI thread.
+  private class AddTask extends AsyncTask<TaskItem, Void, TaskItem> {
+
+    private Context mContext;
+
+    public AddTask(Context context) {
+      mContext = context;
+    }
+
+    @Override
+    protected TaskItem doInBackground(TaskItem... taskItems) {
+      // Items are being added to the list before being scheduled/executed to allow their state to be
+      // visible in the UI.
+      TaskItem taskItem = taskItems[0];
+      CodelabUtil.addTaskItemToFile(mContext, taskItem);
+      return taskItem;
+    }
+
+    @Override
+    protected void onPostExecute(TaskItem taskItem) {
+      mTaskAdapter.addTaskItem(taskItem);
+      mRecyclerView.scrollToPosition(0);
+
+      if (taskItem.getType().equals(TaskItem.ONEOFF_TASK)) {
+        Bundle bundle = new Bundle();
+        bundle.putString(CodelabUtil.TASK_ID, taskItem.getId());
+
+        // Schedule oneoff task.
+        OneoffTask oneoffTask = new OneoffTask.Builder()
+            .setService(BestTimeService.class)
+            .setTag(taskItem.getId())
+            .setRequiredNetwork(OneoffTask.NETWORK_STATE_CONNECTED)
+            // Use an execution window of 30 seconds or more. Less than 30
+            // seconds would not allow GcmNetworkManager enough time to
+            // optimize the next best time to execute your task.
+            .setExecutionWindow(0, 30)
+            .setExtras(bundle)
+            .build();
+        mGcmNetworkManager.schedule(oneoffTask);
+      } else {
+        // Immediately make network call.
+        Intent nowIntent = new Intent(mContext, NowIntentService.class);
+        nowIntent.putExtra(CodelabUtil.TASK_ID, taskItem.getId());
+        mContext.startService(nowIntent);
+      }
+    }
+
+  }
 }
--- network-manager-start/app/src/main/java/com/google/codelab/networkmanager/TaskAdapter.java
+++ network-manager-start/app/src/main/java/com/google/codelab/networkmanager/TaskAdapter.java
@@ -1,12 +1,12 @@
 /**
  * Copyright Google Inc. All Rights Reserved.
- *
- *  Licensed under the Apache License, Version 2.0 (the "License"); you may not use this file except
+ * <p>
+ * Licensed under the Apache License, Version 2.0 (the "License"); you may not use this file except
  * in compliance with the License. You may obtain a copy of the License at
- *
- *  http://www.apache.org/licenses/LICENSE-2.0
- *
- *  Unless required by applicable law or agreed to in writing, software distributed under the
+ * <p>
+ * http://www.apache.org/licenses/LICENSE-2.0
+ * <p>
+ * Unless required by applicable law or agreed to in writing, software distributed under the
  * License is distributed on an "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either
  * express or implied. See the License for the specific language governing permissions and
  * limitations under the License.
@@ -27,123 +27,123 @@ import java.util.List;
 
 public class TaskAdapter extends RecyclerView.Adapter<TaskAdapter.ViewHolder> {
 
-    private static final String TAG = "TaskAdapter";
+  private static final String TAG = "TaskAdapter";
 
-    private List<TaskItem> mTaskItems;
+  private List<TaskItem> mTaskItems;
 
-    public static class ViewHolder extends RecyclerView.ViewHolder {
-        private final TextView mLabelTextView;
-        private final TextView mStatusTextView;
-        private final Button mDeleteButton;
+  public TaskAdapter(List<TaskItem> taskItems) {
+    mTaskItems = taskItems;
+  }
 
-        public ViewHolder(View v) {
-            super(v);
-            mLabelTextView = (TextView) v.findViewById(R.id.taskLabel);
-            mStatusTextView = (TextView) v.findViewById(R.id.taskStatus);
-            mDeleteButton = (Button) v.findViewById(R.id.deleteButton);
-        }
+  @Override
+  public TaskAdapter.ViewHolder onCreateViewHolder(ViewGroup viewGroup, int i) {
+    View v = LayoutInflater.from(viewGroup.getContext()).inflate(R.layout.item_task, viewGroup, false);
+    final ViewHolder viewHolder = new ViewHolder(v);
+    viewHolder.getDeleteButton().setOnClickListener(new View.OnClickListener() {
+      @Override
+      public void onClick(View view) {
+        int position = viewHolder.getAdapterPosition();
+        Log.d(TAG, position + " - " + mTaskItems.size());
+        TaskItem taskItem = mTaskItems.get(position);
+        Log.d(TAG, "Clicked button at position " + position);
+        new DeleteTask(view.getContext()).execute(taskItem);
+      }
+    });
+    return viewHolder;
+  }
 
-        public TextView getLabelTextView() {
-            return mLabelTextView;
-        }
+  @Override
+  public void onBindViewHolder(TaskAdapter.ViewHolder viewHolder, final int position) {
+    final TaskItem taskItem = mTaskItems.get(position);
+    viewHolder.getLabelTextView().setText(taskItem.getType());
+    viewHolder.getStatusTextView().setText(taskItem.getStatus());
+  }
 
-        public TextView getStatusTextView() {
-            return mStatusTextView;
-        }
+  @Override
+  public int getItemCount() {
+    return mTaskItems.size();
+  }
 
-        public Button getDeleteButton() {
-            return mDeleteButton;
-        }
+  public void setTaskItems(List<TaskItem> taskItems) {
+    mTaskItems.clear();
+    mTaskItems.addAll(taskItems);
+    notifyDataSetChanged();
+  }
+
+  public void addTaskItem(TaskItem taskItem) {
+    mTaskItems.add(0, taskItem);
+    notifyItemInserted(0);
+  }
+
+  private int getTaskItemPosition(String id) {
+    for (int i = 0; i < mTaskItems.size(); i++) {
+      TaskItem taskItem = mTaskItems.get(i);
+      if (taskItem.getId().equals(id)) {
+        return i;
+      }
     }
+    return -1;
+  }
 
-    // AsyncTask to delete from file off the UI thread.
-    private class DeleteTask extends AsyncTask<TaskItem, Void, Integer> {
-
-        private Context mContext;
-
-        public DeleteTask(Context context) {
-            mContext = context;
-        }
-
-        @Override
-        protected Integer doInBackground(TaskItem... taskItems) {
-            TaskItem taskItem = taskItems[0];
-            CodelabUtil.deleteTaskItemFromFile(mContext, taskItems[0]);
-            return getTaskItemPosition(taskItem.getId());
-        }
-
-        @Override
-        protected void onPostExecute(Integer position) {
-            if (position == -1) {
-                return;
-            }
-            mTaskItems.remove(position.intValue());
-            notifyItemRemoved(position);
-        }
+  public void updateTaskItemStatus(String id, String status) {
+    for (int i = 0; i < mTaskItems.size(); i++) {
+      TaskItem taskItem = mTaskItems.get(i);
+      if (taskItem.getId().equals(id)) {
+        taskItem.setStatus(status);
+        notifyItemChanged(i);
+        break;
+      }
     }
+  }
 
-    public TaskAdapter(List<TaskItem> taskItems) {
-        mTaskItems = taskItems;
-    }
+  // AsyncTask to delete from file off the UI thread.
+  private class DeleteTask extends AsyncTask<TaskItem, Void, Integer> {
 
-    @Override
-    public TaskAdapter.ViewHolder onCreateViewHolder(ViewGroup viewGroup, int i) {
-        View v = LayoutInflater.from(viewGroup.getContext()).inflate(R.layout.item_task, viewGroup, false);
-        final ViewHolder viewHolder = new ViewHolder(v);
-        viewHolder.getDeleteButton().setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                int position = viewHolder.getAdapterPosition();
-                Log.d(TAG, position + " - " + mTaskItems.size());
-                TaskItem taskItem = mTaskItems.get(position);
-                Log.d(TAG, "Clicked button at position " + position);
-                new DeleteTask(view.getContext()).execute(taskItem);
-            }
-        });
-        return viewHolder;
-    }
+    private Context mContext;
 
-    @Override
-    public void onBindViewHolder(TaskAdapter.ViewHolder viewHolder, final int position) {
-        final TaskItem taskItem = mTaskItems.get(position);
-        viewHolder.getLabelTextView().setText(taskItem.getType());
-        viewHolder.getStatusTextView().setText(taskItem.getStatus());
+    public DeleteTask(Context context) {
+      mContext = context;
     }
 
     @Override
-    public int getItemCount() {
-        return mTaskItems.size();
+    protected Integer doInBackground(TaskItem... taskItems) {
+      TaskItem taskItem = taskItems[0];
+      CodelabUtil.deleteTaskItemFromFile(mContext, taskItems[0]);
+      return getTaskItemPosition(taskItem.getId());
     }
 
-    public void setTaskItems(List<TaskItem> taskItems) {
-        mTaskItems.clear();
-        mTaskItems.addAll(taskItems);
-        notifyDataSetChanged();
+    @Override
+    protected void onPostExecute(Integer position) {
+      if (position == -1) {
+        return;
+      }
+      mTaskItems.remove(position.intValue());
+      notifyItemRemoved(position);
+    }
+  }
+
+  public static class ViewHolder extends RecyclerView.ViewHolder {
+    private final TextView mLabelTextView;
+    private final TextView mStatusTextView;
+    private final Button mDeleteButton;
+
+    public ViewHolder(View v) {
+      super(v);
+      mLabelTextView = (TextView) v.findViewById(R.id.taskLabel);
+      mStatusTextView = (TextView) v.findViewById(R.id.taskStatus);
+      mDeleteButton = (Button) v.findViewById(R.id.deleteButton);
     }
 
-    public void addTaskItem(TaskItem taskItem) {
-        mTaskItems.add(0, taskItem);
-        notifyItemInserted(0);
+    public TextView getLabelTextView() {
+      return mLabelTextView;
     }
 
-    private int getTaskItemPosition(String id) {
-        for (int i = 0; i < mTaskItems.size(); i++) {
-            TaskItem taskItem = mTaskItems.get(i);
-            if (taskItem.getId().equals(id)) {
-                return i;
-            }
-        }
-        return -1;
+    public TextView getStatusTextView() {
+      return mStatusTextView;
     }
 
-    public void updateTaskItemStatus(String id, String status) {
-        for (int i = 0; i < mTaskItems.size(); i++) {
-            TaskItem taskItem = mTaskItems.get(i);
-            if (taskItem.getId().equals(id)) {
-                taskItem.setStatus(status);
-                notifyItemChanged(i);
-                break;
-            }
-        }
+    public Button getDeleteButton() {
+      return mDeleteButton;
     }
+  }
 }
